cc5b50958cd0e1603f8cccac456d4b10fb2222ad
Oliver Bestwalter <oliver@bestwalter.de>
2015-08-25 01:00:05 +0200

    add idempotency bit

--- adfd/tests/test_article.py
+++ adfd/tests/test_article.py
@@ -37,6 +37,8 @@ class TestArticle(object):
         assert a1.slug == 'test-kitchen-sink'
         a2 = Article('test-kitchen-sink', 'some/prefix')
         assert a2.slug == 'some/prefix/test-kitchen-sink'
+        a2a = Article('test-kitchen-sink', 'some/prefix')
+        assert a2a.slug == 'some/prefix/test-kitchen-sink'
         a3 = Article('test-kitchen-sink')
         assert a3.slug == 'test-kitchen-sink'
         assert a3.metadataDict.items() == originalDictItems
