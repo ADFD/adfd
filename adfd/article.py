a67d7c53aca2e0547dfaf768661195d97c10df89
Oliver Bestwalter <oliver@bestwalter.de>
2015-08-24 21:43:26 +0200

    trying a different way to handle the structure

--- adfd/article.py
+++ adfd/article.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
 import logging
 import re
-import plumbum
+from plumbum import LocalPath
 from adfd.utils import ContentGrabber
 
 log = logging.getLogger(__name__)
 
 
-class ENC(object):
-    OUT = 'utf-8'  # used to encode bytes
-
-
 class Article(object):
-    ARTICLES_PATH = plumbum.LocalPath(__file__).up(2) / 'content'
+    ARTICLES_PATH = LocalPath(__file__).up(2) / 'content'
     PUNCT = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.:]+')
 
-    def __init__(self, identifier):
+    def __init__(self, identifier, slugPrefix=None):
         if isinstance(identifier, int):
             self.isImported = True
             self.identifier = "%05d" % (identifier)
@@ -24,9 +21,8 @@ class Article(object):
             self.isImported = False
             self.identifier = identifier
             self.rootPath = self.ARTICLES_PATH / 'static'
-
-        # self.contentPath = self.ARTICLES_PATH / (identifier + '.bb')
-        # self.metadataPath = self.ARTICLES_PATH / (identifier + '.meta')
+        if slugPrefix:
+            self.persist_slug_prefix_modification(slugPrefix)
 
     def __repr__(self):
         return u'<%s %s>' % (self.__class__.__name__, self.slug)
@@ -37,19 +33,33 @@ class Article(object):
             return self.metadataDict['slug']
 
         except KeyError:
-            title = self.metadataDict['title']
-            result = []
-            for word in self.PUNCT.split(title.lower()):
-                word = word.encode('translit/long')
-                if word:
-                    result.append(word)
-            return u'-'.join(result)
+            return self.slugify(self.metadataDict['title'])
+
+    def slugify(self, title):
+        # noinspection PyUnresolvedReferences
+        import translitcodec  # This registers new codecs for slugification
+
+        result = []
+        for word in self.PUNCT.split(title.lower()):
+            word = word.encode('translit/long')
+            if word:
+                result.append(word)
+        return u'-'.join(result)
+
+    def persist_slug_prefix_modification(self, slugPrefix):
+        """writes changed slug back into the file"""
+        # todo prevent writing slugs again and again
+        # todo make sure you write everything back correctly
+        slug = self.slug
+        self.metadataDict['slug'] = "%s/%s" % (slugPrefix, slug)
+        for key, value in self.metadataDict.items():
+            self.metadataPaths[0].write(".. %s: %s" % (key, value))
 
     @property
     def metadataDict(self):
         """only using first found metadata for now ..."""
         metadata = ContentGrabber(absPath=self.metadataPaths[0]).grab()
-        metadataDict = {}
+        metadataDict = OrderedDict()
         for line in metadata.split('\n'):
             if not line.strip():
                 break
--- structure.py
+++ structure.py
@@ -1,44 +1,54 @@
-import collections
-import yaml
-
 from plumbum.path.local import LocalPath
 from adfd.article import Article
 
+structure = [
+    [
+        [Article('kitchen-sink', 'slug/prefix')],
+        'NaviOrdnerName'
+    ],
+    [
+        [Article(68, 'bla'), Article(71, 'blubb')],
+        'AndererNaviOrdnerName'
+    ],
+    [Article(8829), 'blarrr'],
+    [Article(8997), 'absetzhilfen'],
+]
+
 
 class Navigator(object):
-    def __init__(self, structurePath):
-        self.structurePath = structurePath
-        self.map = yaml.safe_load(self.structurePath.read())
+    def __init__(self, structure):
+        self.structure = structure
 
-    def replace_topic_ids(self):
-        for key, value in self.map.items():
-            if isinstance(value, list):
-                self.map[key] = [Article(item) for item in value]
-
-                not isinstance(el, basestring):
-            for sub in flatten(el):
-                yield sub
-        else:
-            yield el
+    def change(self, lol):
+        for idx, item in enumerate(lol):
+            if isinstance(item, list):
+                self.change(item)
+            elif item.startswith('-'):
+                lol[idx] = ['not', item.split('-')[1]]
 
 
 def generate_navigation_links():
-    path = LocalPath(__file__).up() / 'content' / 'structure.yml'
-    navi = Navigator(path)
-    navi.replace_topic_ids()
+    navi = Navigator(structure)
 
+
+# todo think about if we want to use markup in links and if yes: how.
 
 # For regular links: ('https://getnikola.com/', 'Nikola Homepage')
 # submenus: ((('http://a.com/', 'A'), ('http://b.com/', 'O')), 'Fruits')
 # Make sure to end all urls with /
 NAVIGATION_LINKS = {
     'de': generate_navigation_links(),
-    # 'de': (
-    #     ('/archive.html', 'Archiv'),
-    #     ('/categories/', 'Tags'),
-    #     ('/rss.xml', 'RSS-Feed'),
-    # )
+    'en': (
+        (
+            (
+                ('/documentation.html', '<strong>Documentation</strong>'),
+            ),
+            'Documentation'
+        ),
+        ('/blog/index.html', 'Blog'),
+    ),
 }
 
 if __name__ == '__main__':
-    generate_navigation_links()
+    Article('kitchen-sink', 'slug/prefix')
+    # generate_navigation_links()
