3654c72bab20c9337e8a51553ec69ba493ad295b
Oliver Bestwalter <oliver@bestwalter.de>
2015-11-28 01:39:36 +0100

    remove test that is not necessary anymore

--- adfd/tests/test_db_interface.py
+++ adfd/tests/test_db_interface.py
@@ -2,7 +2,6 @@ import os
 
 import pytest
 
-from adfd.cst import DB_URL
 from adfd.db.export import TopicsExporter
 from adfd.db.phpbb_classes import Topic
 
@@ -15,11 +14,6 @@ def is_running_in_ci():
         passenv = CI
     """
     return os.getenv("CI") is not None
-
-
-def test_db_connection_sets_encoding():
-    """This has bitten me and it's not in VCS, so I check it here"""
-    assert DB_URL.endswith('?charset=utf8')
 
 
 @pytest.mark.skipif("is_running_in_ci()")
