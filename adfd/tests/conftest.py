5adb29d3f7ce0dae6f915d5b790599015be7c4bd
Oliver Bestwalter <oliver@bestwalter.de>
2016-02-24 12:24:06 +0100

    set log level in tests to info

--- adfd/tests/conftest.py
+++ adfd/tests/conftest.py
@@ -1,7 +1,8 @@
 import logging
+
 import pytest
 
 
 @pytest.fixture(scope='session', autouse=True)
 def init_tests():
-    logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig(level=logging.INFO)
