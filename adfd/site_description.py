1769026fb79914b9e75e3e4fd0f7d3f7703315cd
Oliver Bestwalter <oliver@bestwalter.de>
2016-03-12 02:08:57 +0100

    update description

--- adfd/site_description.py
+++ adfd/site_description.py
@@ -5,6 +5,12 @@ steckbriefe = CatDesc('Steckbriefe', 10694, [9738, 9735, 9733, 9732, 9731])
 geschichte = CatDesc('Geschichte', 10694, [9913, 9910, 853])
 hintergründe = CatDesc('Hintergründe', 10694, (steckbriefe, geschichte))
 info = CatDesc('Info', 10694, [689, 893])
+linaSammlung = CatDesc(
+    'Linas Liste', 11211,
+    [10199, 2207, 853, 9913, 3462, 7936, 8897, 4694, 8022, 6442, 7663,
+     9910, 9778, 8829, 8621, 9345, 10055, 8507, 11133, 9736, 1896, 3462,
+     11137, 9724, 7819, 9278])
 bbcode = CatDesc('BBcode', 10694, [10068])
 
-SITE_DESCRIPTION = CatDesc('', 10694, (absetzen, hintergründe, info, bbcode))
+SITE_DESCRIPTION = CatDesc(
+    '', 10694, (absetzen, hintergründe, info, linaSammlung, bbcode))
