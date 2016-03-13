e5b726d6dabda3d3ee0f08cc17ee0559a3dd6714
Oliver Bestwalter <oliver@bestwalter.de>
2016-03-14 00:12:46 +0100

    new description

--- adfd/site_description.py
+++ adfd/site_description.py
@@ -9,7 +9,9 @@
 [b]Psychopharmaka[/b] http://adfd.org/austausch/viewtopic.php?f=54&t=11224
 
      - Differentialdiagnose der Depression http://www.adfd.org/austausch/viewtopic.php?f=6&t=9736
-    - 10 Mythen über Psychopharmaka (Götzsche) http://www.adfd.org/austausch/viewtopic.php?f=6&t=10055
+
+     - Psychopharmaka Mythen (TODO Startseite)
+            *10 Mythen über Psychopharmaka (Götzsche) http://www.adfd.org/austausch/viewtopic.php?f=6&t=10055
             *Mythos Serotonin http://www.adfd.org/austausch/viewtopic.php?f=6&t=9799
             *Johanna Moncrieff: Der Mythos der chemischen Heilung http://adfd.org/austausch/viewtopic.php?f=6&t=11133
 
@@ -113,33 +115,36 @@ Legende:[/u]
 """
 from adfd.structure import CatDesc
 
-überUns = CatDesc('Über uns', 10694, [])
+überUns = CatDesc('Über uns', 10694, [11220, 11223, 11222])
 
-_ppAb = CatDesc(
-    'Abhängig von Psychopharmaka?', 10694, [8621, 8829, 11133])
+_ppMyth = CatDesc('Psychopharmaka Mythen', 10694, [10055, 9799, 11133])
+_risiko = CatDesc('Risiken und Nebenwirkungen', 11225, [8676])
 psychopharmaka = CatDesc(
-    'Psychopharmaka', 10694, (10055, 8507, _ppAb, 8676, 9736, 9724))
+    'Psychopharmaka', 11224, [9736, _ppMyth, 8507, _risiko, 10387, 10920])
 
-
-_abAllg = CatDesc(
-    'Allgemeine Tips und Hinweise', 10694, [9913, 7936, 8897])
+_ppAb = CatDesc('Abhängig von Psychopharmaka?', 10694, [8621, 8829, 9724])
+_abAllg = CatDesc('Allgemeine Tips und Hinweise', 10694, [9913, 7936, 8897])
 _abAds = CatDesc('Antidepressiva absetzen', 10694, [853, 9724])
 _abBen = CatDesc('Benzodiazepine absetzen', 10694, [2207, 3462, 4694])
 _ABnL = CatDesc('Neuroleptika absetzen', 10694, [8022, 11137])
 _AbWeit = CatDesc('Weitere Absetzinformationen', 10694, [10199])
-absetzen = CatDesc('Absetzen', 10694, [_abAllg, _abAds, _abBen, _ABnL])
+absetzen = CatDesc('Absetzen', 10694, [_ppAb, _abAllg, _abAds, _abBen, _ABnL])
 
-_entHil = CatDesc('Psychosoziale Hilfen', 10694, [10007])
 _entUm = CatDesc(
-    'Umgang mit Entzugssymptomen', 10694, [9345, 6442, 7663, 9910, 9778])
+    'Umgang mit Entzugssymptomen', 10694,
+    [11218, 9345, 6442, 7663, 9910, 9778])
 _entBeg = CatDesc(
     'Umgang mit Begleit- und Folgeerkrankungen', 10694, [10848])
+_entHil = CatDesc('Psychosoziale Hilfen', 10694, [10007])
 entzug = CatDesc('Hilfe beim Entzug', 10694, [_entUm, _entBeg, _entHil])
 
 
 alternativen = CatDesc('Alternativen', 10694, [7721, 9941, 10940])
+
+medien = CatDesc('Medienberichte und Publikationen', 10694, [10932, 10870])
 dev = CatDesc('[DEV]', 10694, [10068, 11217])
 
 
 SITE_DESCRIPTION = CatDesc(
-    '', 10694, [überUns, psychopharmaka, absetzen, entzug, dev])
+    '', 11221,
+    [überUns, psychopharmaka, absetzen, entzug, alternativen, medien, dev])
