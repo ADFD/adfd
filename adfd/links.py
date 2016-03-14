bc6322b18f6a993c1c60221aa503b96db6563a9a
Oliver Bestwalter <oliver@bestwalter.de>
2016-03-14 21:32:39 +0100

    save status -> merge projects first

--- adfd/links.py
+++ adfd/links.py
@@ -8,8 +8,25 @@ corresponding website article
 * create lookup table from all converted articles
 * convert board links to website links
 """
-from adfd.db.export import harvest_topic_ids
-from adfd.site_description import SITE_DESCRIPTION
 
-topicIds = harvest_topic_ids(SITE_DESCRIPTION)
-print(topicIds)
+
+class LinkInformer:
+    FORUM_LINK_HINT = 'adfd.org/austausch'
+
+    def __init__(self, topics):
+        """
+        :type topics: list Topic
+        """
+        self.topics = topics
+
+    def get_website_link(self, url):
+        if self.FORUM_LINK_HINT not in url:
+            return None
+
+
+if __name__ == '__main__':
+    from adfd.db.export import harvest_topic_ids
+    from adfd.site_description import SITE_DESCRIPTION
+
+    topicIds = harvest_topic_ids(SITE_DESCRIPTION)
+    print(topicIds)
