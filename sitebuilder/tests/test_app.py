b6f5fb2d04d2013ab03f13640d1b4a271b13fc43
Oliver Bestwalter <oliver@bestwalter.de>
2016-03-15 01:12:30 +0100

    tmp rerout into fake path

--- sitebuilder/tests/test_app.py
+++ sitebuilder/tests/test_app.py
@@ -1,4 +1,5 @@
 import pytest
+from plumbum import LocalPath
 
 from sitebuilder.views import app
 
@@ -6,6 +7,8 @@ from sitebuilder.views import app
 @pytest.fixture
 def tc():
     app.config['TESTING'] = True
+    app.config['FLATPAGES_ROOT'] = (
+        str(LocalPath(__file__).dirname.up(2) / 'adfd/tests/data/fake_final'))
     return app.test_client()
 
 
