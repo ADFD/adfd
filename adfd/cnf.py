295f4b95a9416aabb6c30e0e55a8f461af3473c0
Oliver Bestwalter <oliver@bestwalter.de>
2016-09-25 01:20:49 +0200

    ws

--- adfd/cnf.py
+++ adfd/cnf.py
@@ -4,12 +4,14 @@ import yaml
 
 _PROJECT = plumbum.LocalPath(__file__).dirname.up()
 _PACKAGE = _PROJECT / 'adfd'
+
 try:
     _CNF = yaml.safe_load(open(_PACKAGE / 'cnf.yml'))
 except:
     class _CNF:
         def __getitem__(self, item):
             return "dontcare"
+    _CNF = _CNF()
 
 
 class NAME:
