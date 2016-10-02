e042869d31a99603aebef30ac379c47e0e2c8a5b
Oliver Bestwalter <oliver@bestwalter.de>
2016-10-02 19:34:33 +0200

    use local path

--- adfd/cnf.py
+++ adfd/cnf.py
@@ -27,7 +27,7 @@ class PATH:
     SITE = _PACKAGE / 'site'
     STATIC = SITE / NAME.STATIC
     TEMPLATES = SITE / 'view'
-    FROZEN = _CNF['frozenPath'] or _PROJECT / '.frozen'
+    FROZEN = plumbum.LocalPath(_CNF['frozenPath']) or _PROJECT / '.frozen'
 
 
 class APP:
