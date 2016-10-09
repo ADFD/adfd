740c97020676413889aba4597ee31ed5bed3d5af
Oliver Bestwalter <oliver@bestwalter.de>
2016-10-09 02:10:29 +0200

    imports

--- adfd/site/controller.py
+++ adfd/site/controller.py
@@ -2,16 +2,13 @@ import logging
 import os
 import socket
 
-from adfd.utils import date_from_timestamp
 from bs4 import BeautifulSoup
-from flask import Flask, request, render_template
-from flask import flash
-from flask import redirect
-from flask import url_for
+from flask import Flask, request, render_template, flash, redirect, url_for
 from plumbum.machines import local
 
-from adfd.cnf import PATH, SITE, APP, NAME, TARGET, INFO
+from adfd.cnf import PATH, SITE, APP, NAME, INFO
 from adfd.site.navigation import Navigator
+from adfd.utils import date_from_timestamp
 
 log = logging.getLogger(__name__)
 app = Flask(__name__, template_folder=PATH.VIEW, static_folder=PATH.STATIC)
