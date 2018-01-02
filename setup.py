c4491f0d0e853741beb1109304736125f00eb966
Oliver Bestwalter <oliver@bestwalter.de>
2018-01-02 21:02:16 +0100

    quote things

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     name='adfd',
     packages=['adfd'],
     install_requires=open('requirements.txt').readlines(),
-    # This works also in dev mode: pip install -e '.['stats']'
+    # This works also in dev mode: pip install -e '.[stats]'
     extras_require={'stats': ['matplotlib', 'numpy', 'pandas']},
     entry_points=dict(console_scripts=['adfd=adfd.cli:main']))
