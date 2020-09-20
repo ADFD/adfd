cc6fa78856e7e6e1c82c2fba29ae564b3927ec6f
Oliver Bestwalter <oliver@bestwalter.de>
2020-09-20 21:31:07 +0200

    make the renderbending cleaner

--- adfd/site/wsgi.py
+++ adfd/site/wsgi.py
@@ -1,15 +1,14 @@
 import logging
 import os
 
-from flask import Flask, render_template, url_for
-from flask import request, flash, redirect
+import flask
 from plumbum.machines import local
 
 from adfd.cnf import PATH, SITE, APP, NAME, INFO
 from adfd.site.navigation import Navigator
 from adfd.process import date_from_timestamp
 
-app = Flask(__name__, template_folder=PATH.VIEW, static_folder=PATH.STATIC)
+app = flask.Flask(__name__, template_folder=PATH.VIEW, static_folder=PATH.STATIC)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = "I only need that for flash()"
 
@@ -46,9 +45,9 @@ def path_route(path=''):
     assert NAV.pathNodeMap, NAV.pathNodeMap
     node = NAV.get_node(path)
     node.bbcodeIsActive = bbcodeIsActive
-    node.requestPath = request.path
+    node.requestPath = flask.request.path
     # TODO set active path (can be done on node directly)
-    return render_template('content-container.html', node=node)
+    return flask.render_template('content-container.html', node=node)
 
 
 @app.route('/bbcode/article/<topicId>/')
@@ -57,38 +56,38 @@ def path_route(path=''):
 @app.route('/article/<path:path>/')
 def article_route(topicId=None, path=None):
     identifier = topicId or path
-    bbcodeIsActive = request.path[1:].startswith(NAME.BBCODE)
+    bbcodeIsActive = flask.request.path[1:].startswith(NAME.BBCODE)
     try:
         identifier = int(identifier)
     except ValueError:
         pass
     node = NAV.identifierNodeMap[identifier]
     node.bbcodeIsActive = bbcodeIsActive
-    node.requestPath = request.path
-    return render_template('content-container.html', node=node)
+    node.requestPath = flask.request.path
+    return flask.render_template('content-container.html', node=node)
 
 
 @app.route('/check/')
 def check_route():
-    return render_template('check.html', NAV=NAV)
+    return flask.render_template('check.html', NAV=NAV)
 
 
 @app.route('/all-articles/')
 def articles_all_route():
-    return render_template('articles-container.html')
+    return flask.render_template('articles-container.html')
 
 
 @app.route('/reset')
 def reset_route():
     try:
         NAV.populate()
-        flash("navigator repopulated")
+        flask.flash("navigator repopulated")
     except Exception as e:
         app.logger.warning(f"reset failed ({e})")
-    return redirect(url_for(".path_route", path="/")), 301
+    return flask.redirect(flask.url_for(".path_route", path="/")), 301
 
 
-def run_devserver(projectPath=PATH.PROJECT, port=SITE.APP_PORT):
+def run_devserver():
     app.config.update(DEBUG=True)
     fmt = logging.Formatter(
         '%(asctime)s - %(name)s:%(lineno)s [%(process)d|%(thread)d] %(levelname)s: '
@@ -97,19 +96,20 @@ def run_devserver(projectPath=PATH.PROJECT, port=SITE.APP_PORT):
     log = logging.getLogger()
     log.handlers[0].setFormatter(fmt)
     if INFO.IS_DEV_BOX:
+        original_render_template = flask.render_template
+
         def pretty_render_template(template_name_or_list, **context):
             from bs4 import BeautifulSoup
             import flask
-            result = flask.render_template(template_name_or_list, **context)
+            result = original_render_template(template_name_or_list, **context)
             return BeautifulSoup(result, 'html5lib').prettify()
 
-        global render_template
-        render_template = pretty_render_template
+        flask.render_template = pretty_render_template
         os.environ['WERKZEUG_DEBUG_PIN'] = 'off'
-    with local.cwd(projectPath):
+    with local.cwd(PATH.PROJECT):
         host = '0.0.0.0'
-        app.logger.info(f"serving on http://{host}:{port}")
-        app.run(host=host, port=port)
+        app.logger.info(f"serving on http://{host}:{SITE.APP_PORT}")
+        app.run(host=host, port=SITE.APP_PORT)
 
 
 if __name__ == '__main__':
